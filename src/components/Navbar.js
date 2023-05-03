827b66bdbda172a695b36de9480d13556d3bacd7
ErenYeager04 <parkerviktorov094@gmail.com>
2023-05-03 20:27:24 +0200

    production i guess 3

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,14 +28,14 @@ const NavBar = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Container>
         <Link className="navbar-brand" to={SHOP_ROUTE}>E-shop</Link>
-        <button className=" navbar-toggler" type=" button" data-bs-toggle=" collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className=" collapse navbar-collapse justify-content-end" id="navbarNav">
-          <ul className=" navbar-nav">
+          <ul className="navbar-nav ">
             {userId ?
               <>
-                <li className="nav-item">
+                <li className="nav-item active">
                   <Link className="nav-link" to={ADMIN_ROUTE}>Admin</Link>
                 </li>
                 <li className="nav-item">
